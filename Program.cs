184bf10df3d404075bf7235b7ccb454b3e234787
Mnishthakur <102964888+Mnishthakur@users.noreply.github.com>
2023-06-30 15:15:30 +0530

    [Manish Thakur]: Added Program.cs

--- Program.cs
+++ Program.cs
@@ -1,23 +1,51 @@
-namespace AdressBookManagement
-{
-    class Program
-    {
-        static void Main(string[] args)
+// Create a dictionary to store Address Books
+        Dictionary<string, AddressBook> addressBooks = new Dictionary<string, AddressBook>();
 
+        while (true)
         {
-            // Create an instance of the address book
-            AddressBook addressBook = new AddressBook();
+            Console.WriteLine("Select an option:");
+            Console.WriteLine("1. Create a new Address Book");
+            Console.WriteLine("2. Add a contact to an Address Book");
+            Console.WriteLine("3. Display contacts in an Address Book");
+            Console.WriteLine("4. Exit");
+            Console.Write("Enter your choice: ");
+            string choice = Console.ReadLine();
 
-            // Prompt for adding multiple contacts
-            Console.WriteLine("Enter contact details. Enter 'done' to finish.");
-
-            while (true)
+            if (choice == "1")
             {
+                Console.Write("Enter the name for the new Address Book: ");
+                string addressBookName = Console.ReadLine();
+
+                // Check if the Address Book name already exists
+                if (addressBooks.ContainsKey(addressBookName))
+                {
+                    Console.WriteLine("Address Book with the same name already exists. Please choose a different name.");
+                    continue;
+                }
+
+                // Create a new Address Book
+                AddressBook addressBook = new AddressBook();
+                addressBooks.Add(addressBookName, addressBook);
+
+                Console.WriteLine("Address Book created successfully.");
+            }
+            else if (choice == "2")
+            {
+                Console.Write("Enter the name of the Address Book to add a contact: ");
+                string addressBookName = Console.ReadLine();
+
+                // Check if the Address Book exists
+                if (!addressBooks.ContainsKey(addressBookName))
+                {
+                    Console.WriteLine("Address Book not found. Please create the Address Book first.");
+                    continue;
+                }
+
+                AddressBook addressBook = addressBooks[addressBookName];
+
+                Console.WriteLine("Enter contact details:");
                 Console.Write("First Name: ");
                 string firstName = Console.ReadLine();
-
-                if (firstName.ToLower() == "done")
-                    break;
 
                 Console.Write("Last Name: ");
                 string lastName = Console.ReadLine();
@@ -53,16 +81,40 @@ namespace AdressBookManagement
                     Email = email
                 };
 
-                // Add the contact to the address book
+                // Add the contact to the Address Book
                 addressBook.AddContact(contact);
 
-                Console.WriteLine("Contact added successfully.\n");
+                Console.WriteLine("Contact added to the Address Book.");
             }
-            addressBook.DisplayContacts();
+            else if (choice == "3")
+            {
+                Console.Write("Enter the name of the Address Book to display contacts: ");
+                string addressBookName = Console.ReadLine();
+
+                // Check if the Address Book exists
+                if (!addressBooks.ContainsKey(addressBookName))
+                {
+                    Console.WriteLine("Address Book not found. Please create the Address Book first.");
+                    continue;
+                }
+
+                AddressBook addressBook = addressBooks[addressBookName];
+
+                Console.WriteLine("Contacts in Address Book '{0}':", addressBookName);
+                addressBook.DisplayContacts();
+            }
+            else if (choice == "4")
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice. Please select a valid option.");
+            }
+
+            Console.WriteLine();
+            
         }
     }
 
 }
-
-
-
