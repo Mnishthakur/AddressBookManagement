284b63c4e7f4c36c3cd94182f42680b533ca20b1
Mnishthakur <102964888+Mnishthakur@users.noreply.github.com>
2023-06-30 15:26:27 +0530

    [Manish Thakur]: Added Program.cs

--- Program.cs
+++ Program.cs
@@ -5,52 +5,30 @@ namespace AdressBookManagement
         static void Main(string[] args)
 
         {
-            // Create a dictionary to store Address Books
-            Dictionary<string, AddressBook> addressBooks = new Dictionary<string, AddressBook>();
-
             while (true)
             {
-                Console.WriteLine("Select an option:");
+                Console.WriteLine("Address Book System Menu");
                 Console.WriteLine("1. Create a new Address Book");
                 Console.WriteLine("2. Add a contact to an Address Book");
-                Console.WriteLine("3. Display contacts in an Address Book");
-                Console.WriteLine("4. Exit");
+                Console.WriteLine("3. Remove a contact from an Address Book");
+                Console.WriteLine("4. Display contacts in an Address Book");
+                Console.WriteLine("5. Exit");
+
                 Console.Write("Enter your choice: ");
                 string choice = Console.ReadLine();
 
                 if (choice == "1")
                 {
-                    Console.Write("Enter the name for the new Address Book: ");
+                    Console.Write("Enter the name of the new Address Book: ");
                     string addressBookName = Console.ReadLine();
 
-                    // Check if the Address Book name already exists
-                    if (addressBooks.ContainsKey(addressBookName))
-                    {
-                        Console.WriteLine("Address Book with the same name already exists. Please choose a different name.");
-                        continue;
-                    }
-
-                    // Create a new Address Book
-                    AddressBook addressBook = new AddressBook();
-                    addressBooks.Add(addressBookName, addressBook);
-
-                    Console.WriteLine("Address Book created successfully.");
+                    AddressBookSystem.AddAddressBook(addressBookName);
                 }
                 else if (choice == "2")
                 {
-                    Console.Write("Enter the name of the Address Book to add a contact: ");
+                    Console.Write("Enter the name of the Address Book to add the contact: ");
                     string addressBookName = Console.ReadLine();
 
-                    // Check if the Address Book exists
-                    if (!addressBooks.ContainsKey(addressBookName))
-                    {
-                        Console.WriteLine("Address Book not found. Please create the Address Book first.");
-                        continue;
-                    }
-
-                    AddressBook addressBook = addressBooks[addressBookName];
-
-                    Console.WriteLine("Enter contact details:");
                     Console.Write("First Name: ");
                     string firstName = Console.ReadLine();
 
@@ -75,7 +53,6 @@ namespace AdressBookManagement
                     Console.Write("Email: ");
                     string email = Console.ReadLine();
 
-                    // Create a new contact
                     Contact contact = new Contact
                     {
                         FirstName = firstName,
@@ -88,29 +65,29 @@ namespace AdressBookManagement
                         Email = email
                     };
 
-                    // Add the contact to the Address Book
-                    addressBook.AddContact(contact);
-
-                    Console.WriteLine("Contact added to the Address Book.");
+                    addressBookSystem.AddContact(addressBookName, contact);
                 }
                 else if (choice == "3")
+                {
+                    Console.Write("Enter the name of the Address Book to remove the contact: ");
+                    string addressBookName = Console.ReadLine();
+
+                    Console.Write("Enter the First Name of the contact: ");
+                    string firstName = Console.ReadLine();
+
+                    Console.Write("Enter the Last Name of the contact: ");
+                    string lastName = Console.ReadLine();
+
+                    addressBookSystem.RemoveContact(addressBookName, firstName, lastName);
+                }
+                else if (choice == "4")
                 {
                     Console.Write("Enter the name of the Address Book to display contacts: ");
                     string addressBookName = Console.ReadLine();
 
-                    // Check if the Address Book exists
-                    if (!addressBooks.ContainsKey(addressBookName))
-                    {
-                        Console.WriteLine("Address Book not found. Please create the Address Book first.");
-                        continue;
-                    }
-
-                    AddressBook addressBook = addressBooks[addressBookName];
-
-                    Console.WriteLine("Contacts in Address Book '{0}':", addressBookName);
-                    addressBook.DisplayContacts();
+                    addressBookSystem.DisplayAddressBook(addressBookName);
                 }
-                else if (choice == "4")
+                else if (choice == "5")
                 {
                     break;
                 }
@@ -120,12 +97,9 @@ namespace AdressBookManagement
                 }
 
                 Console.WriteLine();
-
             }
         }
     }
-
 }
-
 
 
